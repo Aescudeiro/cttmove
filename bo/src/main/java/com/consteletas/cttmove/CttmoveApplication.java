d6afedf2d82e38368db16a2c1ec0a4f301bf7ab2
Aescudeiro <afonso_escudeiro@hotmail.com>
2020-09-14 21:25:25 +0000

    added spring framework to bo

---
+++ bo/src/main/java/com/consteletas/cttmove/CttmoveApplication.java
@@ -0,0 +1,13 @@
+package com.consteletas.cttmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CttmoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CttmoveApplication.class, args);
+	}
+
+}
---
+++ bo/src/test/java/com/consteletas/cttmove/CttmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consteletas.cttmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CttmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
