0e4dee7635835d9dc62ff3f2e377f8b406e0ad00
Beatriz <beatrizalmeidapires@hotmail.com>
2020-09-15 20:44:56 +0000

    improved logout pop-up

--- fo/src/Components/Popup/Popup.jsx
+++ fo/src/Components/Popup/Popup.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import "./Popup.css";
+import { FaTimes} from 'react-icons/fa';
+
 
 
 const Popup = (props) => {
     return (
         <>
-            <div className="popup">
                 <div className="popup-container">
+                <a id="close" onClick={props.handleClick}><FaTimes size={30}/></a>
                     <p>{props.message}</p>
-                    <button onClick={props.handleClick}>Não</button>
                     <button onClick={props.shut}>Sim</button>
-                </div>
-
             </div>
             <div className="background"></div>
         </>
--- fo/src/Pages/DashBoard/Ajuda/Ajuda.jsx
+++ fo/src/Pages/DashBoard/Ajuda/Ajuda.jsx
@@ -2,20 +2,38 @@ import React from "react";
 import "./Ajuda.css";
 
 const Ajuda = () => {
-  return <div className="help-container">
-    <h1 id="title">Ajuda</h1>
-    <h2>Questões</h2>
-    <h4>Posso pedir um reembolso para um familiar ou um amigo?</h4>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis distinctio accusamus quam voluptates, soluta tenetur quis perspiciatis quod repellendus modi non numquam tempora autem vero facilis mollitia quos fugit consequatur, ad adipisci, maiores totam optio!</p>
-    <h4>Quanto tempo demorará para receber o dinheiro do reembolso?</h4>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti veniam quam dolore eos aliquid aspernatur quos laborum omnis sunt amet</p>
-    <h4>Posso pedir um reembolso num posto CTT?</h4>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, rerum</p>
-    <hr></hr>
-    <h2>Contactar os CTT move</h2>
-    <p>Para esclarecimentos gerais sobre os nossos serviços poderá contactar-nos através do telefone, alternativamente poderá preencher o nosso formulário de contacto.</p>
-    <h1 id="tel">+351 200 000 000</h1><p id="ou">ou</p><button id="contact-form">Formulário de Contacto</button>
-  </div>;
+  return (
+      <div className="help-container">
+        <h1 id="title">Ajuda</h1>
+        <h2>Questões</h2>
+        <h4>Posso pedir um reembolso para um familiar ou um amigo?</h4>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis
+          distinctio accusamus quam voluptates, soluta tenetur quis perspiciatis
+          quod repellendus modi non numquam tempora autem vero facilis mollitia
+          quos fugit consequatur, ad adipisci, maiores totam optio!
+        </p>
+        <h4>Quanto tempo demorará para receber o dinheiro do reembolso?</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          veniam quam dolore eos aliquid aspernatur quos laborum omnis sunt amet
+        </p>
+        <h4>Posso pedir um reembolso num posto CTT?</h4>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, rerum
+        </p>
+        <hr></hr>
+        <h2>Contactar os CTT move</h2>
+        <p>
+          Para esclarecimentos gerais sobre os nossos serviços poderá
+          contactar-nos através do telefone, alternativamente poderá preencher o
+          nosso formulário de contacto.
+        </p>
+        <h1 id="tel">+351 200 000 000</h1>
+        <p id="ou">ou</p>
+        <button id="contact-form">Formulário de Contacto</button>
+      </div>
+  );
 };
 
 export default Ajuda;
