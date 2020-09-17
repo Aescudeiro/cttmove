1b80ce7995bcde4c429f21d1a11c292603e1b34c
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-09-17 20:29:52 +0100

    Web-api basic configs

---
+++ be/ctt-move/ctt-move/Models/Db/CttMoveDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cttMove.Models.Db
+{
+    public partial class CttMoveDbContext : DbContext
+    {
+
+        public CttMoveDbContext ()
+        {
+        }
+
+        public CttMoveDbContext (DbContextOptions<CttMoveDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=Raul;Integrated Security=SSPI;");
+            }
+        }
+
+
+    }
+
+}
