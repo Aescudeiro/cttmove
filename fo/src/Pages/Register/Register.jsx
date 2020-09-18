9684e3b7884562b43d86d727eba5f3e6755dcfb1
Beatriz Pegado Martins de Almeida Pires <Beatriz.P.Pires@globaleda.pt>
2020-09-18 12:03:12 +0000

    improved register form with verifications

--- fo/src/Pages/Register/Register.jsx
+++ fo/src/Pages/Register/Register.jsx
@@ -4,9 +4,13 @@ import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
 import { isEmail } from "validator";
+import { Link } from "react-router-dom";
+import { FaCheck, FaChevronRight } from "react-icons/fa";
 import $ from "jquery";
 
 import AuthService from "../../services/auth-service";
+import { Step } from "@material-ui/core";
+import button from "react-validation/build/button";
 
 const required = (value) => {
   if (!value) {
@@ -18,14 +22,18 @@ const required = (value) => {
 
 const validEmail = (value) => {
   if (!isEmail(value)) {
+    $("#continuar-button").attr("disabled")
     return <h6>Email inválido.</h6>;
   }
+  $("#continuar-button").removeAttr("disabled")
 };
 
 const vpassword = (value) => {
   if (value.length < 6 || value.length > 40) {
+    $("#continuar-button").attr("disabled")
     return <h6>A password deve conter entre 6 e 40 caracteres.</h6>;
   }
+  $("#continuar-button").removeAttr("disabled")
 };
 
 const Register = (props) => {
@@ -42,7 +50,7 @@ const Register = (props) => {
   const [postCode, setPostCode] = useState("");
   const [location, setLocation] = useState("");
   const [message, setMessage] = useState("");
-  const [hide, setHide] = useState(false)
+  const [show, setShow] = useState("step1")
   const onChange = (e, setter) => {
     setter(e.target.value);
   };
@@ -81,34 +89,43 @@ const Register = (props) => {
         }
       );
     }
+
+    setShow("step4")
   };
 
-  const handleClick = () =>{
-    $('.register-form').addClass("hide");
+  const handleClick = (e) => {
+    e.preventDefault()
+    if (show === "step1") {
+      if (!email || !password) return;
+      setShow("step2")
+    } else if (show === "step2") {
+      if (!fullName || !birthDate || !nif || !ccNumber) return;
+      setShow("step3")
+    } else if (show === "step3") {
+      if (!taxAddress || !postCode || !location) return;
+      setShow("step4")
+    }
   }
 
   return (
     <div className="register-container">
-     <div className="pic"></div>
+
+      <div className="picture"></div>
+
+
       <div className="register-form">
-        <Form onSubmit={handleRegister} ref={form}>
-          <h1>Criar Conta</h1>
-          <p id="subtitle">
-            Registe-se e descubra as vantagens que os CTT move lhe podem
-            oferecer.
+        <Form ref={form} id="form" >
+          <div style={{ display: show === "step4" ? "none" : null }}>
+            <h1>Criar Conta</h1>
+            <p id="subtitle">
+              Registe-se e descubra as vantagens que os CTT move lhe podem
+              oferecer.
           </p>
-          <button id="gov-button">AUTENTICAÇÃO.GOV</button>
- 
-            <div>
-              <label htmlFor="fullName">Nome Completo</label>
-              <Input
-                type="text"
-                name="fullName"
-                value={fullName}
-                onChange={(e) => onChange(e, setFullName)}
-                validations={[required]}
-              />
-            </div>
+            <button id="gov-button">AUTENTICAÇÃO.GOV</button>
+          </div>
+
+          <div id="step1" style={{ display: show === "step1" ? null : "none" }}>
+            <h5>STEP 1/3</h5>
 
             <div>
               <label htmlFor="email">Email</label>
@@ -132,14 +149,134 @@ const Register = (props) => {
               />
             </div>
 
+          </div>
+
+          <div id="step2" style={{ display: show === "step2" ? null : "none" }}>
+            <h5>STEP 2/3</h5>
             <div>
-              <button id="continuar-button" onClick={() => setHide(true)}>Continuar</button>
+              <div>
+                <label htmlFor="fullName">Nome Completo</label>
+                <Input
+                  type="text"
+                  name="fullName"
+                  value={fullName}
+                  onChange={(e) => onChange(e, setFullName)}
+                  validations={[required]}
+                />
+              </div>
+
+              <label htmlFor="birthDate">Data de nascimento</label>
+              <Input
+                type="text"
+                name="birthDate"
+                value={birthDate}
+                onChange={(e) => onChange(e, setBirthDate)}
+                validations={[required]}
+              />
             </div>
+            <div id="nif-and-cc-container">
+              <div id="nif">
+                <label htmlFor="nif">NIF</label>
+                <Input
+                  type="text"
+                  name="nif"
+                  value={nif}
+                  onChange={(e) => onChange(e, setNif)}
+                  validations={[required]}
+                />
+              </div>
+
+              <div id="cc">
+                <label htmlFor="ccNumber">Nº Cartão de Cidadão</label>
+                <Input
+                  type="text"
+                  name="ccNumber"
+                  value={ccNumber}
+                  onChange={(e) => onChange(e, setCCNumber)}
+                  validations={[required]}
+                />
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div id="step3" style={{ display: show === "step3" ? null : "none" }}>
+            <h5>STEP 3/3</h5>
+
+            <div>
+              <label htmlFor="taxAddress">Morada Fiscal</label>
+              <Input
+                type="text"
+                name="taxAddress"
+                value={taxAddress}
+                onChange={(e) => onChange(e, setTaxAddress)}
+                validations={[required]}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="postCode">Código Postal</label>
+              <Input
+                type="text"
+                name="postCode"
+                value={postCode}
+                onChange={(e) => onChange(e, setPostCode)}
+                validations={[required]}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="location">Localidade</label>
+              <Input
+                type="text"
+                name="location"
+                value={location}
+                onChange={(e) => onChange(e, setLocation)}
+                validations={[required]}
+              />
+            </div>
+
+
+          </div>
+          <div id="step4" style={{ display: show === "step4" ? null : "none" }}>
+            <div className="final-step-container">
+              <FaCheck size={70} color={"#49BF9C"} />
+              <h2>Registo efectuado com sucesso!</h2>
+              <h5>Por favor confira o seu email para confimar o registo.</h5>
+
+              <button type="button" id="voltar-button">
+                <Link to="/">Voltar ao Início</Link>
+              </button>
+            </div>
+          </div>
+
+          {show !== "step3" && show !== "step4" ?
+            <button id="continuar-button" onClick={(e) => handleClick(e)} disabled={true}>Continuar<FaChevronRight /></button>
+            : show === "step4" ?
+              null
+              :
+              <button id="completar-button" onClick={(e) => { handleRegister(e) }}>Completar Registo<FaChevronRight /></button>
+          }
+
+          <div className="bottom-text" style={{ display: show === "step4" ? "none" : null }}>
+            <p>Já tem conta?<span>
+              <Link to="/login">
+
+                Entre aqui.
+               </Link>
+            </span></p>
+          </div>
+
+
           {message}
           <CheckButton style={{ display: "none" }} ref={checkBtn} />
         </Form>
+
       </div>
-    </div>
+
+    </div >
   );
 };
 
@@ -147,68 +284,6 @@ export default Register;
 
 {
   /* <div>
-          <label htmlFor="birthDate">Data de nascimento</label>
-          <Input
-            type="text"
-            name="birthDate"
-            value={birthDate}
-            onChange={(e) => onChange(e, setBirthDate)}
-            validations={[required]}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="nif">NIF</label>
-          <Input
-            type="text"
-            name="nif"
-            value={nif}
-            onChange={(e) => onChange(e, setNif)}
-            validations={[required]}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="ccNumber">Nº Cartão de Cidadão</label>
-          <Input
-            type="text"
-            name="ccNumber"
-            value={ccNumber}
-            onChange={(e) => onChange(e, setCCNumber)}
-            validations={[required]}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="taxAddress">Morada Fiscal</label>
-          <Input
-            type="text"
-            name="taxAddress"
-            value={taxAddress}
-            onChange={(e) => onChange(e, setTaxAddress)}
-            validations={[required]}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="postCode">Código Postal</label>
-          <Input
-            type="text"
-            name="postCode"
-            value={postCode}
-            onChange={(e) => onChange(e, setPostCode)}
-            validations={[required]}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="location">Localidade</label>
-          <Input
-            type="text"
-            name="location"
-            value={location}
-            onChange={(e) => onChange(e, setLocation)}
-            validations={[required]}
-          />
-        </div> */
+  
+ */
 }
