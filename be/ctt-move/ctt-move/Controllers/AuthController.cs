02afbd4eae7df0bb97eddaebfcaaa7c33d879362
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2020-09-18 16:57:06 +0100

    Data flow architecture implemented and basic Auth logic

--- be/ctt-move/ctt-move/Controllers/AuthController.cs
+++ be/ctt-move/ctt-move/Controllers/AuthController.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using cttMove.Models.Db;
+using cttMove.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace cttMove.Controllers
 {
-    public class AuthController
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
+
+        private AuthService authService;
+
+        public AuthController ()
+        {
+            this.authService = new AuthService();
+        }
+
+        [HttpPost("login")]
+        public IActionResult login ([FromBody] CttUser userData)
+        {
+            CttUser loggedUser = authService.login(userData.Email, userData.Pass);
+
+            if (loggedUser == null)
+            {
+                return BadRequest();
+            }
+            loggedUser.Pass = ""; //Should improve this using DTOs;
+            return Ok(loggedUser);
+        }
     }
 }
---
+++ be/ctt-move/ctt-move/Models/DAL/AuthRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cttMove.Models.Db;
+using Microsoft.AspNetCore.Builder;
+
+namespace cttMove.Models.DAL
+{
+    public class AuthRepository
+    {
+        private CttMoveDbContext dbContext;
+
+        public AuthRepository ()
+        {
+            this.dbContext = new CttMoveDbContext();
+        }
+
+        public CttUser getUser(string email)
+        {
+            return dbContext.CttUser.Where(u => u.Email == email).FirstOrDefault();
+        }
+    }
+}
